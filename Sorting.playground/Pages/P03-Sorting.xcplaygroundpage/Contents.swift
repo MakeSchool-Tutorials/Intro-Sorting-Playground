4e361f292495aa921c99bc68d3ec94886f73bf78
Dion Larson <me@dionlarson.com>
2016-06-10 20:17:18 -0700

    Rewrite P03

--- Sorting.playground/Pages/P03-Sorting.xcplaygroundpage/Contents.swift
+++ Sorting.playground/Pages/P03-Sorting.xcplaygroundpage/Contents.swift
@@ -2,96 +2,98 @@ import XCPlayground
 let deck = Deck(comparator: valueOnlyComparator)
 let (sceneView, scene) = CardSortScene.setupScene(deck)
 /*:
-Don't worry about the code here – this sets up the deck of cards that we'll be manipulating, and the scene that displays it.
-*/
-
+ 
+ Don't worry about the code here – this sets up the deck of cards that we'll be manipulating, and the scene that displays it.
+ 
+ */
 //: [Previous](@previous)
-
 /*:
-# Our First Sorting Algorithm: Bubble Sort!
-
-For our first sorting algorithm, we're going to start with a simple algorithm called "__Bubble Sort__."
-
-Before we start, we haven't yet said anything about how to retrieve information from the `Card` structs that inhabit our array. You can retrive the number `value` of a card, like so:
-
-    let firstCard = cards[0]
-    let value = firstCard.value
-
-Similarly, you can retrieve the suite of a card, like this:
-
-    let suite = firstCard.suite
-
-We won't be dealing with suites just yet, though. Anyway, back to Bubble Sort...
-
-The idea behind Bubble Sort is to repeatedly "bubble" up the larger elements towards the end of array, so that eventually the entire array will be sorted in ascending order. Sound like magic? Let's try it:
-
-Let's say you look at the first two cards of the deck. You compare the two cards: does the first card have the higher value? _If_ so, we'll _swap_ those two cards, using the handy function we wrote earlier. Now those two cards should be in ascending order!
-
-    let first = 0
-    let second = 1
-    if cards[first].value > cards[second].value {
-        swap(first, second)
-    }
-
-Well, great. Now what about the rest of the cards? Worry not! We'll go ahead and do the same thing for the next pair of cards. And the next pair. And the next...until we reach the end of the array. Like so:
-
-    for index in 0...(cards.count-2) {
-        if (cards[index].value > cards[index+1].value {
-            swap(index, index+1)
+ 
+ # Accessing Card values
+ 
+ Before we start, we should show you how to retrieve information from the `Card` _structs_ that make up our array. You can retrive the number `value` of a card like this:
+ 
+     let firstCard = cards[0]
+     let value = firstCard.value
+ 
+ Similarly, you can retrieve the suite of a card, like this:
+ 
+     let suite = firstCard.suite
+ 
+ We won't be dealing with suites just yet, though. You can continue to treat them all as equal!
+ 
+ # Our First Sorting Algorithm: Bubble Sort!
+ 
+ The idea behind _Bubble Sort_ is to repeatedly "bubble" up the larger elements towards the end of array! We do this over and over again until our `Array` is sorted. Sound like magic?
+ 
+ Before we get into coding this, let's check it out in dance form [here](https://www.youtube.com/watch?v=lyZQPjUT5B4)!
+ 
+ - callout(Challenge): The sorting magic starts _53 seconds_ into the video but you should just start from the beginning. As you watch, try to write out pseudocode for the algorithm on pen and paper. Save this so you can compare it to the pseudocode we give you later in this section!
+ 
+ ## How it works
+ 
+ Let's imagine you inspect the first two cards of the deck. You compare the two cards -- does the first card have the higher value? _If_ it does, _swap_ the two cards. Now those two cards are in ascending order!
+ 
+     if card 0 is less card 1
+         swap card 0 and card 1
+ 
+ Well, great. Now what about the rest of the cards? Worry not! We'll go ahead and do the same thing for the next pair of cards. And the next pair. And the next... until we reach the end of the array.
+ 
+    for index from 0 to (number of cards - 2)
+        if card index is less than card (index + 1)
+            swap card index and card (index + 1)
+ 
+ ### Why does the for loop go from (number of cards - 2)?
+ 
+ - callout(Solution): Remember that array indices go from `0` to `count - 1`, and that we're inspecting pairs of `index` and `index + 1`. If we iterate `index` from `0` to `count - 1`, `index + 1` would be outside the array bounds!
+ 
+ ## Code it out!
+ 
+ Go ahead and write translate the above pseudocode into Swift! Does it sort your list?
+ 
+ - callout(Remember): You can loop over values in a range with:
+ 
+        for counterVariable in startValue...endValue {
+ 
         }
-    }
-
-A note here: Why does the for loop go from 0 to cards.count-2? Remember that array indices go from `0` to `count-1`, and that we're inspecting pairs of `index` and `index+1`. If we iterate `index` from `0` to `count-1`, `index+1` would be outside the array bounds!
-
-Go ahead and write this code in our new `bubblesort` function, provided below.
-
-*/
+ 
+ */
 extension Deck {
-    func bubblesort() {
-        //Student code here!
-        //TODO: remove code
-        for end in (cards.count-2).stride(to:1, by: -1) {
-            for index in 0...end {
-                if cards[index].value > cards[index+1].value {
-                    swap(index, index+1)
-                }
-            }
-        }
+    func bubbleSort() {
+        
+        
+        
     }
 }
-deck.bubblesort()
-
+deck.bubbleSort()
 /*:
-
-Well, that didn't sort the deck completely. But you'll notice, that now the largest card has "bubbled" to the end of the array! If we do this multiple times, we'll get the next largest card to be at one before the end, the next next largest card to be two before the end, and so on. Note that when you go through this process the second time, you don't need to compare the last two cards, since the last card is already in place. This also holds true for the next iteration: in the third run, the last two cards are in place, and so on...
-
-Can you modify our `bubblesort()` function and give it its complete implementation? Hint: It involves wrapping our existing code into _another_ for loop, and then modifying the variables that determine when the "bubbling" process ends.
-
-*/
-
-/*:
-Congratulations, your array is now sorted! But perhaps you're not satisfied, because this sorting algorithm didn't seem very efficient. It sort of defied common sense too, since when you typically move the largest card to the end of the hand, you _scan_ through the pile looking for the largest value, instead of comparing each card against its neighbor and doing this bubbling process. We'll explore this room for improvmeent in the next section, where we will introduce a faster sorting algorithm, called Selection Sort. See you there!
-
-*/
-
-
+ 
+ ### It's still not sorted! :(
+ 
+ Well, that didn't sort the deck completely. But you'll notice, that the largest card has "bubbled" to the end of the array! If we do this multiple times, we'll get the next largest card to be at one before the end, the next next largest card to be two before the end, and so on.
+ 
+ When you go through this process the second time, you don't need to compare the last two cards, since the last card is already in place. This also holds true for the next iteration: in the third run, the last two cards are in place, and so on...
+ 
+ - callout(Challenge): Translate the full implementation into pseudocode! How close was your pseudocode from when you were watching the video?
+ 
+ ## Moment of truth
+ 
+ - callout(Challenge): Modify the `bubbleSort()` function above to finish sorting the cards!
+ 
+ - callout(Hint): It involves wrapping our existing code into _another_ for loop, and then modifying the variables that determine when the "bubbling" process ends.
+ 
+    You can reverse a range with `.reverse()`. `(1...5)` would produce `1, 2, 3, 4, 5` but `(1...5).reverse()` produces `5, 4, 3, 2, 1`.
+ 
+ # Did you do it?
+ 
+ Congratulations, your array is now sorted! How do you feel about _Bubble Sort_? Is it how you would sort a deck of cards?
+ 
+ Perhaps you're not satisfied. Maybe this sorting algorithm didn't seem very efficient to you. It sort of defies common sense. When you are sorting cards by hand you probably move the largest card to the end of the hand and then repeat the process. You likely _scan_ through the pile looking for the largest value, instead of comparing each card against its neighbor and doing this bubbling process. We'll explore this room for improvmeent in the next section and introduce a faster sorting algorithm called Selection Sort.
+ 
+ */
 //: [Next](@next)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /*:
-Don't worry about this code. This takes the scene and deck information and displays it.
-*/
+ Don't worry about this code. This takes the scene and deck information and displays it.
+ */
 sceneView.presentScene(scene);
 XCPlaygroundPage.currentPage.liveView = sceneView;
