d111041a04622dac19a2ef0c77355784587013b1
Dion Larson <me@dionlarson.com>
2016-06-10 20:16:45 -0700

    Rewrite P02

--- Sorting.playground/Pages/P02-Swapping.xcplaygroundpage/Contents.swift
+++ Sorting.playground/Pages/P02-Swapping.xcplaygroundpage/Contents.swift
@@ -2,50 +2,57 @@ import XCPlayground
 let deck = Deck(comparator: nil)
 let (sceneView, scene) = CardSortScene.setupScene(deck)
 /*:
-Don't worry about the code here – this sets up the deck of cards that we'll be manipulating, and the scene that displays it.
-*/
-
+ 
+ Don't worry about the code here – this sets up the deck of cards that we'll be manipulating, and the scene that displays it.
+ 
+ */
 //: [Previous](@previous)
 
-
 /*:
-## Swapping
-
-_Swapping_ two elements in an array sounds pretty simple: You take the element at the first chosen index in the array, and the element at the second chosen index, and assign them to eachothers' places. Here, we've written out such a swap function for you, but its implementation is wrong. Can you find what's wrong and fix it?
-
-*/
+ 
+ # Swapping
+ 
+ _Swapping_ two elements in an `Array` sounds pretty simple. You take the _element_ at the first index, the _element_ at the second index, and switch their places. Here, we've written out such a swap function for you, but its implementation is wrong. Can you find what's wrong and fix it?
+ 
+ */
 extension Deck {
 /*:
-Note: Inside this class, the variable `cards` is an array of `Card` types (`Card` is a struct that we've defined ourselves, with two Int parameters, `suite` and `value`). You can access elements of this variable to manipulate the order of the cards.
-*/
+     
+ - note: Inside this class, the variable `cards` is an array of `Card` types. `Card` is a `struct` that we've defined ourselves, with two `Int` parameters -- `suite` and `value`.
+     
+    A _struct_ is a lot like a _class_. It can define it's own methods and properties but it cannot use inheritance. There are a few other differences that instructors can help you with but they are not important for this exercise!
+ 
+ */
+
     public func swap(first: Int, _ second: Int) {
-        
-        // Student code here!
         cards[first] = cards[second]
         cards[second] = cards[first]
     }
+    
 /*:
-Hint: You can save a value from an array to a _temporary_ variable by declaring and initializing it with `let x = ...`.
-*/
+     
+ - callout(Hint): If it says _Copying_ in the preview then you did not do it right! You might need to create a new variable to store one of the values during the swap!
+     
+ */
 }
+/*:
+ 
+ Now in order to test our swap function, we'll call it from the special `Deck` object that we've created for you. Simply call `deck.swap(x, y)` with two integers between 0 and 12 (since we have 13 cards in our array).
+ 
+     deck.swap(0, 1)
+ 
+ - important: Remember that array indices start go from 0 to _one less than_ the length of the array!
+ 
+ */
+
+deck.swap(4, 1)
 
 /*:
-
-Now in order to test our swap function, we'll call it from the special `Deck` object that we've made for you. Simply call `deck.swap(x, y)`, passing in two integers between 0 and 12 (since we have 13 cards in our array), like so:
-
-    deck.swap(0, 1)
-
-Remember that array indices start go from 0 to _one less than_ the length of the array!
-*/
-
-// Student code here!
-deck.swap(1, 3)
-
-
-/*:
-Once you're done, hit the Next button to move on to the next section, where we'll be writing our first sorting algorithm using the swap function!
-*/
-
+ 
+ - callout(Challenge): The `Deck` _on this page_ will be the same every time you run the code. Can you get it sorted? Treat _Aces_ as `1`, _Jacks_ as `11`, _Queens_ as `12`, and _Kings_ as `13`. _Suite_ (or the symbol on the card) does not matter right now).
+ 
+ Once the deck is sorted, hit the `Next` button to move on to the next section. We'll teach you _Bubblesort_, a foolproof sorting algorithm that uses the swap function!
+ */
 //: [Next](@next)
 
 
@@ -62,8 +69,7 @@ Once you're done, hit the Next button to move on to the next section, where we'l
 
 
 /*:
-Don't worry about this code. This takes the scene and deck information and displays it.
-*/
+ Don't worry about this code. This takes the scene and deck information and displays it.
+ */
 sceneView.presentScene(scene);
 XCPlaygroundPage.currentPage.liveView = sceneView;
-
