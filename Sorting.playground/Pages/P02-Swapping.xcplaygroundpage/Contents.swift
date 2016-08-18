205d7715fe918829eeab22ce5259a4d751ba02dc
Jorrie Brettin <jorriebrettin@gmail.com>
2016-08-18 15:45:58 +0530

    High level comments added

--- Sorting.playground/Pages/P02-Swapping.xcplaygroundpage/Contents.swift
+++ Sorting.playground/Pages/P02-Swapping.xcplaygroundpage/Contents.swift
@@ -2,25 +2,24 @@ import XCPlayground
 let deck = Deck(comparator: nil, seed: false)
 let (sceneView, scene) = CardSortScene.setupScene(deck)
 /*:
- 
+
  Don't worry about the code here – this sets up the deck of cards that we'll be manipulating, and the scene that displays it.
  ![Make School Banner](./swift_banner.png)
  # Swapping
- 
+
  _Swapping_ two elements in an `Array` sounds pretty simple. You take the _element_ at the first index, the _element_ at the second index, and switch their places. Here, we've written out such a swap function for you, but its implementation is wrong. Can you find what's wrong and fix it?
- 
+
  */
 extension Deck {
 /*:
-     
+
  - note: Inside this class, the variable `cards` is an array of `Card` types. `Card` is a `struct` that we've defined ourselves, with two `Int` parameters -- `suit` and `value`.
-     
+
     A _struct_ is a lot like a _class_. It can define it's own methods and properties but it cannot use inheritance. There are a few other differences that instructors can help you with but they are not important for this exercise!
- 
+
  */
 
     public func swap(_ first: Int, _ second: Int) {
-        // Fix this!
         //First, make copies of both cards
         let firstCard = cards[first]
         let secondCard = cards[second]
@@ -28,22 +27,23 @@ extension Deck {
         cards[first] = secondCard
         cards[second] = firstCard
     }
-    
+
 /*:
-     
+
  - callout(Hint): If it says _Copying_ in the preview then you did not do it right! You might need to create a new variable to store one of the values during the swap!
-     
+
  */
 }
 /*:
- 
+
  Now in order to test our swap function, we'll call it from the special `Deck` object that we've created for you. Simply call `deck.swap(x, y)` with two integers between 0 and 12 (since we have 13 cards in our array).
- 
+
      deck.swap(0, 1)
- 
+
  - important: Remember that array indices start go from 0 to _one less than_ the length of the array!
- 
+
  */
+ <!-- TODO: Add some kind of test to see if their swap func works. --!>
 
 deck.swap(4, 1)
 
@@ -53,9 +53,10 @@ CANNOT IMPLEMENT CHALLENGE
 */
 
 /*:
- 
+
  - callout(Challenge): The `Deck` _on this page_ will be the same every time you run the code. Can you get it sorted? Treat _Aces_ as `1`, _Jacks_ as `11`, _Queens_ as `12`, and _Kings_ as `13`. _Suit_ (or the symbol on the card) does not matter right now).
- 
+ <!-- TODO: Add room for them to work here. In all other places, students read the challenge, then work below it. The room above is working double duty right now. --!>
+
  Once the deck is sorted, hit the `Next` button to move on to the next section. We'll teach you _Bubblesort_, a foolproof sorting algorithm that uses the swap function!
  */
 //: [Previous](@previous) | [Next](@next)
--- Sorting.playground/Pages/P06-Merge-Sort.xcplaygroundpage/Contents.swift
+++ Sorting.playground/Pages/P06-Merge-Sort.xcplaygroundpage/Contents.swift
@@ -1,31 +1,31 @@
 /*:
  ![Make School Banner](./swift_banner.png)
  # Merge Sort: A divide-and-conquer algorithm
- 
+
  So far, we've implemented _iterative_ sorting algorithms – ones that go through each of the elements in the array and run some operation on each of them. This time, we're going to look at our first _recursive_ sorting algorithm: _Merge Sort_. The algorithm is split into two parts: divide & conquer (or merge).
- 
+
  ## Divide
- 
+
  1. Divide an `Array` into two new `Array`s at the middle
  2. Continue doing this divide until you cannot do it anymore
- 
+
  ## Conquer (or merge)
- 
+
  Since we've called `mergeSort` on both chunks already, we assume that each chunk has been independently sorted. Now, all we need to do is to _merge_ these two sorted chunks together into one sorted chunk.
- 
+
  ## Visual representation
- 
+
  This algorithm makes a lot more sense when seen visually (from [Wikipedia's page on Merge Sort](https://en.wikipedia.org/wiki/Merge_sort)):
  ![](mergesort.gif)
- 
- ## I'm still a bit confused...
- 
- Let's check in with the dancers again! Check it out in action [here](https://www.youtube.com/watch?v=XaqR3G_NVoo).
- 
- - callout(Crazy Challenge): Try writing out the pseudocode for _Merge Sort_ and compare it with a partner. Get a deck of cards and try it out your pseudocode with 10 cards (`Ace` through `Jack`).
- 
- */
 
+ ## I'm still a bit confused...
+
+ Let's check in with the dancers again! Check it out in action [here](https://www.youtube.com/watch?v=XaqR3G_NVoo).
+
+ - callout(Crazy Challenge): Try writing out the pseudocode for _Merge Sort_ and compare it with a partner. Get a deck of cards and try it out your pseudocode with 10 cards (`Ace` through `Jack`).
+
+ */
+//Pseudocode
 //mergeSort(list){
 //base case:
 //if list.count <= 1
@@ -36,6 +36,7 @@
 //merge the two lists
 //return that merged list
 
+//Real code
 let numCardsInSuit = 13
 
 public func <(first: Card, second: Card) -> Bool {
@@ -43,15 +44,17 @@ public func <(first: Card, second: Card) -> Bool {
     return first.suit.rawValue * numCardsInSuit + first.value < second.suit.rawValue * numCardsInSuit + second.value
 }
 
+<!-- TODO: I think we should at least give them the option of doing this on their own here. Yes, the pseudocode and then running through the algo should be a challenge, but the `Crazy challenge` would be to code it. I also think they should be given the tools to do so (I had to copy and paste the above code such that mine would work). Even if they can't right away, this could be something they think about when they're going about their day away from computer - obsessing over how to code it. I think it could inspire some people to really dive into the problem and discover recursion on their own along the way. --!>
+
 extension Deck{
-    
+
     func merge_sort(){
         cards = merge_sort(inputCards: cards)
     }
-    
+
     private func merge_sort(inputCards: [Card]) -> [Card]{
         let sortMe = inputCards
-        
+
         //Base case
         if sortMe.count <= 1{
             return sortMe
@@ -64,9 +67,9 @@ extension Deck{
         let sortedLeft = merge_sort(inputCards: left)
         let sortedRight = merge_sort(inputCards: right)
         return merge(sortedLeft, sortedRight)
-        
+
     }
-    
+
     private func merge(_ left:[Card],_ right:[Card]) -> [Card]{
         var mergedCards = [Card]()
         var mutableLeft = left
@@ -88,7 +91,7 @@ extension Deck{
     }
 }
 
-
+<!-- TODO: Add an additional wrap-up section where runtime complexity is either explained in broad strokes or we link them to another tutorial... something they can look into on their own, maybe? I admit it'll be hard to talk run time with beginners on merge sort. --!>
 
 import XCPlayground
 let deck = Deck(comparator: suitFirstComparator)
